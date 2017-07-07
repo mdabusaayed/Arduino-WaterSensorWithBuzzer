5cc4249f69036c6d0e8515bf65231c49e1c9b2b3
Mohammad Abu Sayed <mdabusaayed@gmail.com>
2017-07-08 02:04:58 +0600

    Add files via upload

---
+++ water_code.ino
@@ -0,0 +1,27 @@
+const int analogInPin = A0;
+int sensorValue = 450;
+int outputValue = 112;
+int spPin = 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(spPin,OUTPUT);
+}
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  outputValue = map(sensorValue, 450, 1023, 112, 255);
+  Serial.print("sensor=  ");
+  Serial.print(sensorValue);
+  Serial.print("\t liquid point= ");
+  Serial.println(outputValue);
+  delay(500);
+  if(sensorValue>0){
+    for (int i=0; i<1000; i++)
+  {
+    digitalWrite(spPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(spPin, LOW);
+    delayMicroseconds(500);
+    
+   }
+  }
+}
